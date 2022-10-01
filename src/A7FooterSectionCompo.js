096ea116990dce5233599e4fb76f3eb223744197
shivam92211 <80212879+shivam92211@users.noreply.github.com>
2022-10-01 13:48:37 +0530

    Updated personal contacts with dummy

--- src/A7FooterSectionCompo.js
+++ src/A7FooterSectionCompo.js
@@ -21,10 +21,10 @@ export default class A7FooterSectionCompo extends Component {
                                 <div className="d-flex justify-content-center mx-auto col-lg-5 col-md-7 col-12">
                                     <p className="mr-4">
                                         <i className="fa fa-envelope-o mr-1"></i>
-                                        <a href="#">hello@company.co</a>
+                                        <a href="#">fakemail@company.co</a>
                                     </p>
 
-                                    <p><i className="fa fa-phone mr-1"></i>+91 7977951858</p>
+                                    <p><i className="fa fa-phone mr-1"></i>+91 9876543210</p>
                                 </div>
 
                             </div>
